a4f0068d4a560359f2de2643170d445b6c87f9eb
RebuildEQ <RebuildEQ@gmail.com>
2024-03-21 08:00:36 +0000

    Update issue

--- lua_modules/commands/issue.lua
+++ lua_modules/commands/issue.lua
@@ -58,15 +58,101 @@ local function issue(e)
     msg = msg .. "-------\n"
 
     msg = msg .. "inventory\n"
-    for i = 0, 30 do
+
+    --- a dictionary of slots
+    local slots = {
+        [Slot.Primary] = "Primary",
+        [Slot.Secondary] = "Secondary",
+        [Slot.Charm] = "Charm",
+        [Slot.Ear1] = "Ear1",
+        [Slot.Head] = "Head",
+        [Slot.Face] = "Face",
+        [Slot.Ear2] = "Ear2",
+        [Slot.Neck] = "Neck",
+        [Slot.Shoulders] = "Shoulders",
+        [Slot.Arms] = "Arms",
+        [Slot.Back] = "Back",
+        [Slot.Wrist1] = "Wrist1",
+        [Slot.Wrist2] = "Wrist2",
+        [Slot.Range] = "Range",
+        [Slot.Hands] = "Hands",
+        [Slot.Finger1] = "Finger1",
+        [Slot.Finger2] = "Finger2",
+        [Slot.Chest] = "Chest",
+        [Slot.Legs] = "Legs",
+        [Slot.Feet] = "Feet",
+        [Slot.Waist] = "Waist",
+        [Slot.PowerSource] = "PowerSource",
+        [Slot.Ammo] = "Ammo",
+        [Slot.Cursor] = "Cursor",
+        [Slot.Shoulder] = "Shoulder",
+        [Slot.Bracer1] = "Bracer1",
+        [Slot.Bracer2] = "Bracer2",
+        [Slot.Ring1] = "Ring1",
+        [Slot.Ring2] = "Ring2",
+    }
+
+    for slot, name in pairs(slots) do
+        local item = inv:GetItem(slot)
+        if item ~= nil and item.valid then
+            msg = string.format("%s%s: %s (%d)\n", msg, name, item:GetName(), item:GetID())
+        else
+            msg = string.format("%s%s: none\n", msg, name)
+        end
+    end
+
+    for i = Slot.PossessionsBegin, Slot.PossessionsEnd do
         local item = inv:GetItem(i)
-        if item.valid then
-            msg = string.format("%s%s (%d)\n", msg, item:GetName(), item:GetID())
+        if item ~= nil and item.valid then
+            msg = string.format("%sPossession %d: %s (%d)\n", msg, i, item:GetName(), item:GetID())
+        else
+            --msg = string.format("%sPossession %d: none\n", msg, i)
+        end
+    end
+
+    for i = Slot.PossessionsBagsBegin, Slot.PossessionsBagsEnd do
+        local item = inv:GetItem(i)
+        if item ~= nil and item.valid then
+            msg = string.format("%sPossession Bag %d: %s (%d)\n", msg, i, item:GetName(), item:GetID())
+        else
+            --msg = string.format("%sPossession Bag %d: none\n", msg, i)
         end
     end
 
 
+    for i = Slot.CursorBagBegin, Slot.CursorBagEnd do
+        local item = inv:GetItem(i)
+        if item ~= nil and item.valid then
+            msg = string.format("%sCursor Bag %d: %s (%d)\n", msg, i, item:GetName(), item:GetID())
+        else
+            --msg = string.format("%sCursor Bag %d: none\n", msg, i)
+        end
+    end
 
+    local item = inv:GetItem(Slot.Tradeskill)
+    if item ~= nil and item.valid then
+        msg = string.format("%sTradeskill: %s (%d)\n", msg, item:GetName(), item:GetID())
+    else
+        msg = string.format("%sTradeskill: none\n", msg)
+    end
+
+    for slot = Slot.BankBagsBegin, Slot.BankBagsEnd do
+        local item = inv:GetItem(slot)
+        if item ~= nil and item.valid then
+            msg = string.format("%sBank Bag %d: %s (%d)\n", msg, slot, item:GetName(), item:GetID())
+        else
+            --msg = string.format("%sBank Bag %d: none\n", msg, slot)
+        end
+    end
+
+    for slot = Slot.SharedBankBagsBegin, Slot.SharedBankBagsEnd do
+        local item = inv:GetItem(slot)
+        if item ~= nil and item.valid then
+            msg = string.format("%sShared Bank Bag %d: %s (%d)\n", msg, slot, item:GetName(), item:GetID())
+        else
+            --msg = string.format("%sShared Bank Bag %d: none\n", msg, slot)
+        end
+    end
 
     local filename = "issues/" .. e.self:AccountName() .. "-" .. math.random(1000000, 9999999) .. ".txt";
 
