8775880fcfca8f3ce1958b3a590a96a0b73a01e1
RebuildEQ <RebuildEQ@gmail.com>
2024-04-02 11:13:29 +0000

    Add SetAAExp and SetEXP

--- mods/exp.lua
+++ mods/exp.lua
@@ -1,14 +1,18 @@
----@param e ModGetExperienceForKill
-function GetExperienceForKill(e)
+---@param e ModSetExp
+function SetEXP(e)
     if not e.self:IsClient() then
         return e
     end
     local client = e.self:CastToClient()
 
-    local experience = require('experience')
-
-    local base_exp = experience.get_base(e.other)
-    base_exp = base_exp * 0.6499999761581
+    eq.debug(string.format("exp source: %d, cur: %d, set: %d, diff: %d", e.exp_source, e.current_exp, e.set_exp, e.set_exp - e.current_exp))
+    if e.exp_source ~= ExpSource.Quest and
+        e.exp_source ~= ExpSource.Kill and
+        e.exp_source ~= ExpSource.LDoNChest and
+        e.exp_source ~= ExpSource.Task then
+            eq.debug("ignoring exp source")
+        return e
+    end
 
     local multiplier = 1
     local boost_4x = e.self:GetBucket("boost_4x")
@@ -17,7 +21,7 @@ function GetExperienceForKill(e)
     end
 
     local boost_catchup = e.self:GetBucket("boost_catchup")
-    if boost_catchup == "ON" and e.self:GetLevel() <= 50 then
+    if boost_catchup == "ON" and e.self:GetLevel() <= 50 and e.exp_source == ExpSource.Kill then
         local level = client:GetLevel()
         multiplier = multiplier + 6
         if level >= 10 then
@@ -37,15 +41,74 @@ function GetExperienceForKill(e)
         end
     end
 
-    local final_exp = base_exp * multiplier
+    local base_gain = e.set_exp - e.current_exp
+    local new_gain = base_gain*multiplier
+    e.return_value = e.current_exp + new_gain
 
     local is_debug = e.self:GetBucket("exp_debug")
 
     if is_debug == "ON" then
-        e.self:Message(MT.Experience, string.format("Base EXP: %d, Multiplier: x%d, Before Con/Split Final: %d", base_exp, multiplier, final_exp))
+        e.self:Message(MT.Experience, string.format("Exp Mod (%d * %0.0f) = %d", base_gain, multiplier, new_gain))
     end
 
-    e.ReturnValue = final_exp
-    e.IgnoreDefault = true
+    e.ignore_default = true
+    return e
+end
+
+---@param e ModSetAAExp
+function SetAAExp(e)
+    if not e.self:IsClient() then
+        return e
+    end
+    local client = e.self:CastToClient()
+    local total_aa = client:GetSpentAA() + client:GetAAPoints()
+
+    eq.debug(string.format("aa exp source: %d, cur: %d, set: %d, diff: %d", e.exp_source, e.current_exp, e.set_exp, e.set_exp - e.current_exp))
+    if e.exp_source ~= ExpSource.Quest and
+        e.exp_source ~= ExpSource.Kill and
+        e.exp_source ~= ExpSource.LDoNChest and
+        e.exp_source ~= ExpSource.Task then
+            eq.debug("ignoring exp source")
+        return e
+    end
+
+    local multiplier = 1
+    local boost_4x = e.self:GetBucket("boost_4x")
+    if boost_4x == "ON" then
+        multiplier = multiplier * 4
+    end
+
+    local boost_catchup = e.self:GetBucket("boost_catchup")
+    if boost_catchup == "ON" and total_aa <= 100 and e.exp_source == ExpSource.Kill then
+        local level = client:GetLevel()
+        multiplier = multiplier + 6
+        if level >= 10 then
+            multiplier = multiplier + 4
+        end
+        if level >= 20 then
+            multiplier = multiplier + 4
+        end
+        if level >= 30 then
+            multiplier = multiplier + 4
+        end
+        if level >= 40 then
+            multiplier = multiplier + 6
+        end
+        if level >= 50 then
+            multiplier = multiplier + 10
+        end
+    end
+
+    local base_gain = e.set_aa_exp - e.current_aa_exp
+    local new_gain = base_gain*multiplier
+    e.return_value = e.current_aa_exp + new_gain
+
+    local is_debug = e.self:GetBucket("exp_debug")
+
+    if is_debug == "ON" then
+        e.self:Message(MT.Experience, string.format("Exp Mod (%d * %0.0f) = %d", base_gain, multiplier, new_gain))
+    end
+
+    e.ignore_default = true
     return e
 end
--- mods/mighty.lua
+++ mods/mighty.lua
@@ -101,6 +101,20 @@ function HealDamage(e)
         multiplier = 2
     end
 
+    local spell_name = "unknown"
+
+    local spell = eq.get_spell(e.spell_id)
+    if spell then
+        spell_name = spell:Name()
+    end
+
+    if eq.is_lifetap_spell(e.spell_id) then
+        multiplier = 1
+    end
+    if e.spell_id == 65535 then
+        multiplier = 1
+    end
+
     local is_debug = ally:GetBucket("mighty_debug")
 
     e.return_value = e.value * multiplier
@@ -108,14 +122,60 @@ function HealDamage(e)
         return e
     end
 
-    ally:Message(MT.Spells, string.format("Outgoing mighty (%d * %0.1f) = %d", e.value, multiplier, e.return_value))
+    ally:Message(MT.Spells, string.format("Incoming mighty heal %s (%d * %0.1f) = %d", spell_name, e.value, multiplier, e.return_value))
     return e
 end
-
 
 ---@param e ModCommonDamage
 function CommonDamage(e)
     local multiplier = 1
+
+    local dt_spells = {
+        982, -- cazic touch
+        1948, -- Destroy
+        2156, -- Deadly Curse of Noqufiel
+        7599, -- Gargoyle Glance
+        8436, -- Touch of Terror
+        8999, -- Runic Backlash
+        9019, -- Backlash of Rage
+        9272, -- Avalanche's Torrent of Ice
+        9273, -- Glacial Assault
+        9376, -- Touch of the Phantom Effect
+        12465, -- Infinite Pain
+        13031, -- Boom
+        13318, -- Harvest Fungus Patch Doom
+        17816, -- Scalding Steam Blast
+        17832, -- Whirring Copter Blades
+        21761, -- Focused Rage
+        27522, -- Barrier of Insanity
+        27939, -- Battle Orb Explosion
+        32450, -- Soul Shatter
+        32531, -- Corruptive Crud Direct Damage
+        32974, -- Stab of Chaos
+        37550, -- Icerain Explosion
+        39055, -- Frozen Dread
+        39066, -- Fright Force II
+        39083, -- Cazic's Displeasure
+        39103, -- Eruption
+        39180, -- Death Touch
+        39224, -- Cold Shock
+        42046, -- Bleeding Out
+        42049, -- Dehydrated
+        42050, -- Demoralized
+        42071, -- Raging Tempest
+        42161, -- Blazebeam
+    }
+
+    for _, v in pairs(dt_spells) do
+        if e.spell_id == v and e.self:IsClient() then
+            e.self:Message(MT.Say, string.format("%s just tried to death touch you, but jamfest nerfed it to 100 damage.", e.attacker:GetCleanName()));
+            e.return_value = 100
+            e.ignore_default = true
+            return e
+        end
+    end
+
+
 
     if e.value < 0 then
         return e
@@ -169,15 +229,143 @@ function CommonDamage(e)
     local is_debug = ally:GetBucket("mighty_debug")
 
     e.return_value = e.value * multiplier
+    e.ignore_default = true
 
     if not is_debug or is_debug ~= "ON" then
         return e
     end
 
     if is_good then
-        ally:Message(MT.Spells, string.format("Outgoing mighty (%d * %0.1f) = %d", e.value, multiplier, e.return_value))
+        ally:Message(MT.Spells, string.format("Outgoing mighty damage (%d * %0.1f) = %d", e.value, multiplier, e.return_value))
         return e
     end
-    ally:Message(MT.Spells, string.format("Incoming mighty (%d * %0.1f) = %d", e.value, multiplier, e.return_value))
+    ally:Message(MT.Spells, string.format("Incoming mighty damage (%d * %0.1f) = %d", e.value, multiplier, e.return_value))
+    return e
+end
+
+---@param e ModResistSpellRoll
+function ResistSpellRoll(e)
+    local is_good = false
+    local ally = e.self
+    local enemy = e.caster
+    if not ally.valid then
+        return e
+    end
+    if not enemy.valid then
+        return e
+    end
+    if not ally:IsClient() then
+        if ally:HasOwner() and ally:GetOwner():IsClient() then
+            ally = ally:GetOwner() -- pet
+        else
+            is_good = true
+            ally = e.caster
+            enemy = e.self
+            if not ally:IsClient() then
+                if not ally:HasOwner() then
+                    return e
+                end
+                if not ally:GetOwner():IsClient() then
+                    return e
+                end
+                ally = ally:GetOwner() -- pet
+            end
+        end
+    end
+
+
+    local multiplier = 1
+    local mighty = ally:GetBucket("boost_mighty")
+    if mighty == "ON" then
+        if is_good then
+            if enemy:IsNPC() then
+                --multiplier = 2
+                multiplier = 1
+                if enemy:CastToNPC():IsRaidTarget() then
+                    --multiplier = 4
+                end
+            end
+        else
+            multiplier = 0.4
+            if enemy:IsNPC() and enemy:CastToNPC():IsRaidTarget() then
+                multiplier = 0.3
+            end
+        end
+    end
+
+    local is_debug = ally:GetBucket("mighty_debug")
+
+    e.return_value = e.roll * multiplier
+    e.ignore_default = true
+    if e.return_value > e.roll_max then
+        e.return_value = e.roll_max
+    end
+
+
+    if not is_debug or is_debug ~= "ON" then
+        return e
+    end
+
+    local is_overrode = false
+    local overrode_text = ""
+    if not is_good and e.return_value > e.resist_chance then
+        local roll = math.random(1, 100)
+        if roll <= 50 then
+            is_overrode = true
+            overrode_text = ", override to " .. e.resist_chance
+        end
+    end
+
+
+    local banish_spells = {
+        853, --	Burning Touch II
+        854, --	Burning Touch
+        788, --	Reality Warp
+        866, --	Verdict of the Tribunal
+        1087, --	Mechinetic Dislocation I
+        1088, --	Mechinetic Dislocation II
+        1089, --	Mechinetic Dislocation III
+        1090, --	Mechinetic Dislocation IV
+        1091, --	Mechinetic Dislocation V
+        1092, --	Mechinetic Dislocation VI
+        1093, --	Mechinetic Dislocation VII
+        1094, --	Mechinetic Dislocation VIII
+        1095, --	Mechinetic Dislocation IX
+        1096, --	Mechinetic Dislocation X
+        1124, --	Penance of Flame
+        1125, --	Penance of the Whip
+        1127, --	Penance of Execution
+        1128, --	Penance of Stone
+        1129, --	Penance of Torture
+        1164, --	Crusader's Banishment
+        1476, --	The Dain's Justice
+    }
+
+    for _, v in pairs(banish_spells) do
+        if e.spell_id == v and e.self:IsClient() then
+            e.self:Message(MT.Spells, string.format("%s just tried to banish you, but jamfest resisted it.", e.caster:GetCleanName()));
+            e.return_value = e.resist_chance
+            e.ignore_default = true
+            return e
+        end
+    end
+
+
+    local landed = "landed"
+    if e.return_value < e.resist_chance or is_overrode then
+        landed = "resisted"
+    end
+
+    if is_good then
+        ally:Message(MT.Spells, string.format("Outgoing mighty resist (%d * %0.1f) = %d%s vs %d (%s)", e.roll, multiplier, e.return_value, overrode_text, e.resist_chance, landed))
+        if is_overrode then
+            e.return_value = e.resist_chance
+        end
+        return e
+    end
+    ally:Message(MT.Spells, string.format("Incoming mighty resist (%d * %0.1f) = %d%s vs %d (%s)", e.roll, multiplier, e.return_value, overrode_text, e.resist_chance, landed))
+    if is_overrode then
+        e.return_value = e.resist_chance
+    end
     return e
 end
