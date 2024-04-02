22548adf407b37d882bd47464e856f0e698e8048
RebuildEQ <RebuildEQ@gmail.com>
2024-04-02 12:35:20 +0000

    Fix SetAAEXP casing

--- mods/exp.lua
+++ mods/exp.lua
@@ -1,4 +1,4 @@
----@param e ModSetExp
+---@param e ModSetEXP
 function SetEXP(e)
     if not e.self:IsClient() then
         return e
@@ -55,15 +55,16 @@ function SetEXP(e)
     return e
 end
 
----@param e ModSetAAExp
-function SetAAExp(e)
+---@param e ModSetAAEXP
+function SetAAEXP(e)
+
     if not e.self:IsClient() then
         return e
     end
     local client = e.self:CastToClient()
     local total_aa = client:GetSpentAA() + client:GetAAPoints()
 
-    eq.debug(string.format("aa exp source: %d, cur: %d, set: %d, diff: %d", e.exp_source, e.current_exp, e.set_exp, e.set_exp - e.current_exp))
+    eq.debug(string.format("aa exp source: %d, cur: %d, set: %d, diff: %d", e.exp_source, e.current_aa_exp, e.set_aa_exp, e.set_aa_exp - e.current_aa_exp))
     if e.exp_source ~= ExpSource.Quest and
         e.exp_source ~= ExpSource.Kill and
         e.exp_source ~= ExpSource.LDoNChest and
@@ -106,7 +107,7 @@ function SetAAExp(e)
     local is_debug = e.self:GetBucket("exp_debug")
 
     if is_debug == "ON" then
-        e.self:Message(MT.Experience, string.format("Exp Mod (%d * %0.0f) = %d", base_gain, multiplier, new_gain))
+        e.self:Message(MT.Experience, string.format("AA Exp Mod (%d * %0.0f) = %d", base_gain, multiplier, new_gain))
     end
 
     e.ignore_default = true
