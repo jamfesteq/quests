1cd662b97abe175a974dbb32de53c0b63bef2c26
Xackery <xackery@gmail.com>
2024-04-07 21:18:32 -0700

    Fix typo

--- global/global_npc.lua
+++ global/global_npc.lua
@@ -226,7 +226,7 @@ function do_buffs_and_ports(e)
     elseif (mob_class == 31) then wizard_ports(e)
     elseif (mob_class == 33) then enchanter_buffs(e)
     elseif (e.self:GetNPCTypeID() == 345004) then wizard_ports(e) -- porter in guildhall
-    elseif (eq.get_zone_id() == 344 and strings.contains(e.self:GetCleanName(), "Guardian")) then wizard_ports(e) -- guards in guildlobby
+    elseif (eq.get_zone_id() == 344 and e.self:GetCleanName().find("Guardian")) then wizard_ports(e) -- guards in guildlobby
     end
 end
 
